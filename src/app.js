17e36d40ed691683fedd8dfe809b76a00387a808
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-07-06 02:38:31 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let action = ["run", "danced"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let b = 0; b < noun.length; b++) {
+      for (let c = 0; c < action.length; c++) {
+        document.querySelector("#domain").innerHTML +=
+          "<p>" + pronoun[i] + adj[a] + noun[b] + action[c] + ".com</p>";
+      }
+    }
+  }
+}
